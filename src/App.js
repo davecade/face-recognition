4970740dc43d48f5436f21d338c05a27597f5d24
Dave <dave.cadelina@outlook.com>
2021-10-12 23:23:25 +1100

    completed the styling of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
+import './App.scss';
+import Navigation from './components/Navigation/Navigation.component'
+import Logo from './components/Logo/Logo.component'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.component'
+import Rank from './components/Rank/Rank.component'
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density:  {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation />
+      <Particles 
+            params={particlesOptions}
+            className="particles"
+      />
+      <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <nav className="navbar">
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation
