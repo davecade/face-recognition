1f324da2516f23f171c91f4259eb21eedf9943f2
Dave <dave.cadelina@outlook.com>
2021-10-16 11:01:02 +1100

    face api fetch working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react'
 import './App.scss';
 import Navigation from './components/Navigation/Navigation.component'
 import Logo from './components/Logo/Logo.component'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.component'
 import Rank from './components/Rank/Rank.component'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
 
 const particlesOptions = {
   particles: {
@@ -17,7 +20,24 @@ const particlesOptions = {
   }
 }
 
-function App() {
+const app = new Clarifai.App({
+  apiKey: '0a0461ea46f64dca854a666d16349850'
+ });
+
+
+const App = () => {
+  const [ input, setInput ] = useState('')
+
+
+  const onInputChange = event => {
+    setInput(event.target.value)
+  }
+//https://samples.clarifai.com/face-det.jpg
+  const onButtonSubmit = async (e) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, "https://samples.clarifai.com/face-det.jpg")
+    .then( resp => console.log(resp))
+  }
+
   return (
     <div className="App">
       <Particles 
@@ -27,7 +47,8 @@ function App() {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+      <div>{input}</div>
       {/* <FaceRecognition /> */}
     </div>
   );
