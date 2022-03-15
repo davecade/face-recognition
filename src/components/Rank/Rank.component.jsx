332f6dafc1dff1de5a285b209acef3951ff80dfc
Dave <dave.cadelina@outlook.com>
2022-03-15 16:51:07 +1100

    works with lambda function

--- src/components/Rank/Rank.component.jsx
+++ src/components/Rank/Rank.component.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Rank = ({ name, entries }) => {
+
+    const [ emoji, setEmoji ] = useState("")
+
+    const generateEmoji = async entries => {
+        try {
+            const response = await fetch(`https://8owsjbn958.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+            const data = await response.json()
+            console.log(data)
+            setEmoji(data.input)
+        } catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        generateEmoji(entries)
+    }, [entries])
+
     return (
         <div>
             <div className='white f3'>
@@ -8,6 +26,9 @@ const Rank = ({ name, entries }) => {
             </div>
             <div className='white f1'>
                 {entries}
+            </div>
+            <div className='white f1'>
+                {`Rank Badge: ${emoji}`}
             </div>
       </div>
     )
